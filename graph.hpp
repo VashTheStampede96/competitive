d948d206eac5240797691e7353bc04139959462d
vash <brian.riccardi96@gmail.com>
2018-02-28 17:57:03 +0100

    added Kosaraju

--- graph.hpp
+++ graph.hpp
@@ -1,4 +1,8 @@
+#ifndef _GRAPH_HASHCODE_HPP_
+#define _GRAPH_HASHCODE_HPP_
+
 #include <bits/stdc++.h>
+using namespace std;
 
 template<class T>
 class Graph {
@@ -17,6 +21,39 @@ private:
         }
     }
 
+    void discorec(int u, vector<int>& discovery, vector<bool>& vis) {
+        vis[u] = true;
+
+        for(auto v: adj[u]) {
+            if(not vis[v.label])
+                discorec(v.label, discovery, vis);
+        }
+        
+        discovery.push_back(u);
+    }
+
+    void component(int u, int leader, vector<bool>& vis, vector<int>& leaders) {
+        leaders[u] = leader;
+        vis[u] = true;
+
+        for(auto v: adj[u]) {
+            if(not vis[v])
+                component(v.label, leader, vis, leaders);
+        }
+    }
+
+    vector<int> assign_leaders(vector<int>& sorted) {
+        vector<bool> vis(nodes, false);
+        vector<int> leaders(nodes, -1);
+
+        for(auto u: sorted) {
+            if(not vis[u])
+                component(u, u, vis, leaders);
+        }
+
+        return leaders;
+    }
+
 public:
     static const int oo;
 
@@ -24,13 +61,15 @@ public:
         adj.resize(n);
     }
 
+    // *************************************** ADD EDGE **********************
     void add_edge(int u, T v) {
         adj[u].push_back(v);
         if(not directed)
             adj[v].push_back(u);
     }
 
-    Graph transpose() {
+    // *************************************** TRANSPOSE *********************
+    Graph<T> transpose() {
         Graph tr(nodes, edges, directed);
 
         for(int u=0; u<nodes; ++u) {
@@ -44,20 +83,14 @@ public:
         return tr;
     }
 
-    vector<int> Dijkstra(bool w, int source, int end=-1) {
+    // *************************************** DIJKSTRA **********************
+    vector<int> Dijkstra(int source, int end=-1) {
         vector<int> distance(nodes, oo);
-        if(w)
-            priority_queue<pair<int,int>> q;
-        else
-            queue<pair<int, int>> q;
+        priority_queue<pair<int,int>> pq;
 
         pq.push({0, source});
-        while(!pq.empty()) {
-            int d;
-            if(w)
-                d = -pq.top().first;
-            else
-                d = -pq.front().first;
+        while(not pq.empty()) {
+            int d = -pq.top().first;
             int u = pq.top().second;
 
             if(d < distance[u]) {
@@ -65,7 +98,7 @@ public:
                 
                 for(auto v: adj[u]) {
                     if(d+v.weight < distance[v.label])
-                        pq.push({-(d+weight), v.label});
+                        pq.push({-(d+v.weight), v.label});
                 }
             }
 
@@ -76,6 +109,34 @@ public:
         return distance;
     }
 
+    // *************************************** BFS ***************************
+    vector<int> bfs(int source, int end=-1) {
+        vector<bool> vis(nodes, false);
+        vector<int> distance(nodes, oo);
+        queue<pair<int,int>> q;
+
+        q.push({0, source});
+        while(not q.empty()) {
+            int d = q.front().first;
+            int u = q.front().second;
+
+            if(d < distance[u]) {
+                distance[u] = u;
+
+                for(auto v: adj[u]) {
+                    if(d+1 < distance[v.label])
+                        q.push({d+1, v.label});
+                }
+            }
+
+            if(u == end)
+                break;
+        }
+
+        return distance;
+    }
+
+    // *************************************** DFS ***************************
     vector<int> dfs() {
         vector<bool> vis(nodes, false);
         vector<int> distance(nodes, oo);
@@ -88,15 +149,32 @@ public:
         return distance;
     }
 
-    vector<int> Kusaraju() {
+    // *************************************** DFS+SORTING *******************
+    vector<int> dfs_discovery() {
+        vector<bool> vis(nodes, false);
+        vector<int> discovery;
+
+        for(int u=0; u<nodes; ++u) {
+            if(not vis[u])
+                discorec(u, discovery, vis);
+        }
+
+        return discovery;
+    }
+
+    // *************************************** KOSARAJU **********************
+    vector<int> Kosaraju() {
         Graph tr = transpose();
 
-        vector<int> dfs1 = dfs();
-        vectorGraph tr = transpose();
-        
-        vector<int> dfs1 = dfs();
+        vector<int> disc = dfs_discovery();
+        reverse(disc.begin(), disc.end());
+
+        return tr.assign_leaders(disc);
     }
 
 };
 
-Graph:: const int oo = 1e9;
+template<class T>
+const int Graph<T>::  oo = 1e9;
+
+#endif
